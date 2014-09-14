3bf94b4ed02c8114b7d5c2eb5abfce6c714eb312
Martin Cameron <mumart@gmail.com>
2014-09-14 21:56:54 +0000

    Add basic save wave support and improve vibrato.

--- jvst/examples/liquinth/Synthesizer.java
+++ jvst/examples/liquinth/Synthesizer.java
@@ -2,6 +2,7 @@
 package jvst.examples.liquinth;
 
 public interface Synthesizer {
+	public int getSamplingRate();
 	public int setSamplingRate( int samplingRate );
 	public char getVersion();
 	public void noteOn( int key, int velocity );
